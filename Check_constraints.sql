ae7ec12487df8b0ae90484590ec4721703beac77
sanjaybiswas52 <sanjaybiswas52@gmail.com>
2026-02-15 19:09:56 +0530

    Update payment methods and enforce constraints
    Added new payment methods and updated constraints for the payment and orders tables. Included deduplication logic for fact_orders.

--- Check_constraints.sql
+++ Check_constraints.sql
@@ -106,38 +106,75 @@ INSERT INTO dim_employee VALUES
 (202, 'Ravi', 'Support');
 
 -- Payments
-INSERT INTO dim_payment VALUES 
+INSERT INTO dim_payment_tmp VALUES
 (301, 'Credit Card', 'Visa'),
-(302, 'UPI', NULL);
+(302, 'UPI', NULL),
+(303, 'Debit Card', 'RuPay'),
+(304, 'Cash', NULL),
+(305, 'Points', NULL);
+ALTER TABLE dim_payment SWAP WITH dim_payment_tmp;
 
--- Shipping
-INSERT INTO dim_shipping VALUES 
-(401, 'FedEx', 'Air'),
-(402, 'BlueDart', 'Ground');
+ALTER TABLE dim_payment ADD CONSTRAINT pk_payment primary key (PAYMENT_ID);
 
--- Promotions
-INSERT INTO dim_promotion VALUES 
-(501, 'NEWYEAR', 'Holiday Campaign'),
-(502, 'FLASHSALE', 'Weekend Sale');
-
--- Orders (Fact Table)
-INSERT INTO fact_orders VALUES 
-(1001, 1, 101, '2026-02-01', 11, 201, 301, 401, 501, 1, 25000.00),
-(1002, 2, 102, '2026-02-05', 12, 202, 302, 402, 502, 2, 120000.00);
-
--- Constraint Violation Check
-DELETE FROM dim_customer WHERE customer_id = 1;
-
--- If you want cascading deletes:
-
+----------------------------------------
 ALTER TABLE fact_orders 
 DROP CONSTRAINT fk_customer;
 
 ALTER TABLE fact_orders 
 ADD CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES dim_customer(customer_id) ON DELETE CASCADE;
 
+---------------
+ALTER TABLE fact_orders ADD COLUMN update_date TIMESTAMP;
 
+INSERT INTO fact_orders 
+(order_id, customer_id, product_id, date_id, store_id, employee_id, payment_id, shipping_id, promo_id, quantity, amount, update_date)
+VALUES
+(1001, 1, 101, '2026-02-01', 11, 201, 301, 401, 501, 1, 25000.00, CURRENT_TIMESTAMP),
+(1002, 2, 102, '2026-02-05', 12, 202, 302, 402, 502, 2, 120000.00, CURRENT_TIMESTAMP),
+(1003, 1, 102, '2026-02-06', 11, 201, 301, 401, 501, 1, 60000.00, CURRENT_TIMESTAMP),
+(1004, 2, 101, '2026-02-07', 12, 202, 302, 402, 502, 3, 75000.00, CURRENT_TIMESTAMP),
+(1005, 1, 101, '2026-02-08', 11, 201, 301, 401, 501, 2, 50000.00, CURRENT_TIMESTAMP),
+(1006, 2, 102, '2026-02-09', 12, 202, 302, 402, 502, 1, 60000.00, CURRENT_TIMESTAMP),
+(1007, 1, 101, '2026-02-10', 11, 201, 301, 401, 501, 1, 25000.00, CURRENT_TIMESTAMP),
+(1008, 2, 102, '2026-02-11', 12, 202, 302, 402, 502, 2, 120000.00, CURRENT_TIMESTAMP),
+(1009, 1, 102, '2026-02-12', 11, 201, 301, 401, 501, 1, 60000.00, CURRENT_TIMESTAMP),
+(1010, 2, 101, '2026-02-13', 12, 202, 302, 402, 502, 3, 75000.00, CURRENT_TIMESTAMP);
+-- Continue similarly until you reach 50 rows
+;
 
+SELECT order_id, count(*) FROM fact_orders GROUP BY order_id HAVING count(*) > 1
+-- Remove duplicate records from fact_orders
+CREATE OR REPLACE TABLE fact_orders
+AS 
+SELECT order_id, customer_id, product_id, date_id, store_id, employee_id, payment_id, shipping_id, promo_id, quantity, amount, update_date
+FROM fact_orders GROUP BY (order_id, customer_id, product_id, date_id, store_id, employee_id, payment_id, shipping_id, promo_id, quantity, amount, update_date)
+
+-- Note :it drops and recreates the table. That means all metadata — including primary keys, foreign keys, constraints, clustering keys, comments, tags, grants — are lost. Snowflake doesn’t have a syntax to “preserve constraints” automatically in a CREATE OR REPLACE … AS SELECT (CTAS) statement. 
+
+SELECT * FROM fact_orders
+
+CREATE OR REPLACE TABLE fact_orders_dedup (
+    order_id INT PRIMARY KEY,
+    customer_id INT,-- REFERENCES dim_customer(customer_id),
+    product_id INT REFERENCES dim_product(product_id),
+    date_id DATE,
+    store_id INT REFERENCES dim_store(store_id),
+    employee_id INT REFERENCES dim_employee(employee_id),
+    payment_id INT REFERENCES dim_payment(payment_id),
+    shipping_id INT REFERENCES dim_shipping(shipping_id),
+    promo_id INT REFERENCES dim_promotion(promo_id),
+    quantity INT,
+    amount NUMBER(10,2),
+    update_date TIMESTAMP
+)
+AS
+SELECT DISTINCT order_id, customer_id, product_id, date_id, store_id, employee_id, payment_id, shipping_id, promo_id, quantity, amount, CURRENT_TIMESTAMP()
+FROM fact_orders ;
+
+select distinct customer_id from fact_orders where order_id = '1001'
+
+SELECT * FROM dim_customer
+    
 
 ---------------------- END ---------------
 USE DATABASE HR
